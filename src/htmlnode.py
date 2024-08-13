cd29a6f3cb9e2fdb8c2f676fe7502069097568c5
Cory Burmahl <beenters@gmail.com>
2024-08-13 13:31:33 -0700

    Added Leaf Node with Unit Tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,3 +27,18 @@ class HTMLNode:
             and self.children == other.children
             and self.props == other.props
         )
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        if tag is None and value is None:
+            raise ValueError("At least one of tag or value must be specified")
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        elif self.props is None:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 from textnode import TextNode
 
 class TestHTMLNode(unittest.TestCase):
@@ -15,6 +15,14 @@ class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
         node = HTMLNode("div", "world", [TextNode("hello", "text", url="https://boot.dev")], {"href": "https://boot.dev", "target": "_blank"})
         self.assertEqual(repr(node), "HTMLNode(div, world, [TextNode(hello, text, https://boot.dev)], {'href': 'https://boot.dev', 'target': '_blank'})")
+
+    def test_to_html(self):
+        node = LeafNode("div", "world")
+        self.assertEqual(node.to_html(), "<div>world</div>")
+    
+    def test_to_html2(self):
+        node = LeafNode("div", "world", {"href": "https://boot.dev", "target": "_blank"})
+        self.assertEqual(node.to_html(), '<div href="https://boot.dev" target="_blank">world</div>')
 
 if __name__ == "__main__":
     unittest.main()
