d9f1d1f468ae7cc10f77c2d303e56d16df6a991e
Cory Burmahl <beenters@gmail.com>
2024-08-13 12:56:23 -0700

    Initial bones of Static Site Generator

---
+++ main.sh
@@ -0,0 +1,3 @@
+python src/main.py
+
+# TODO: add a script to run this script in a docker container
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+def main():
+    textnode = TextNode("hello world", "text", url="https://boot.dev")
+    print("Printing TextNode:")
+    print(textnode)
+
+if __name__ == "__main__":
+    main()
