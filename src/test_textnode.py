6971cd20a7061c738f48498a8af0b985c6ac7b03
Cory Burmahl <beenters@gmail.com>
2024-08-13 14:10:22 -0700

    Added text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -26,6 +26,51 @@ class TestTextNode(unittest.TestCase):
     def test_repr(self):
         node = TextNode("hello world", "text", url="https://boot.dev")
         self.assertEqual(repr(node), "TextNode(hello world, text, https://boot.dev)")
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("hello world", "text")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "hello world")
+
+    def test_bold(self):
+        node = TextNode("hello world", "bold")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "hello world")
+    
+    def test_italic(self):
+        node = TextNode("hello world", "italic")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "hello world")
+
+    def test_code(self):
+        node = TextNode("hello world", "code")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "hello world")
+    
+    def test_link(self):
+        node = TextNode("hello world", "link", url="https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "hello world")
+        self.assertEqual(html_node.props["href"], "https://boot.dev")
+
+    def test_image(self):
+        node = TextNode("hello world", "image", url="https://boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://boot.dev", "alt": "hello world"},
+        )
+
+
 
 if __name__ == "__main__":
     unittest.main()
