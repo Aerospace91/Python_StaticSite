3d1b72570080bcfa6597b13037f357b010a00e0a
Cory Burmahl <beenters@gmail.com>
2024-08-14 07:31:24 -0700

    block to block types

---
+++ src/markdown_blocks.py
@@ -0,0 +1,52 @@
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return "heading"
+
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].endswith("```"):
+        return "code"
+    
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return "paragraph"
+        return "quote"
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return "paragraph"
+        return "unordered_list"
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return "paragraph"
+        return "unordered_list"
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return "paragraph"
+            i += 1
+        return "ordered_list"
+
+    return "paragraph"
+        
+    
